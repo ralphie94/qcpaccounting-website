6deb4f5cb2c9772cdf26a5c3aa3850e117c45a69
Ralphie Celedon <ralphie94@pacbell.net>
2022-01-19 21:57:22 -0800

    created dropdown for services link in navbar

---
+++ client/src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: "Tax",
+        path: "/tax",
+        cName: "dropdown-link"
+    },
+    {
+        title: "Bookkeeping",
+        path: "/bookkeeping",
+        cName: "dropdown-link"
+    },
+    {
+        title: "Insurance",
+        path: "/insurance",
+        cName: "dropdown-link"
+    },
+    {
+        title: "Paralegal",
+        path: "/paralegal",
+        cName: "dropdown-link"
+    }
+]
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import Dropdown from "./Dropdown";
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const onMouseEnter = () => {
+        if(window.innerWidth < 960) {
+            setDropdown(false); 
+        } else {
+            setDropdown(true);
+        }
+    };
+
+    const onMouseLeave = () => {
+        if(window.innerWidth < 960) {
+            setDropdown(false); 
+        } else {
+            setDropdown(false);
+        }
+    };
 
     return (
         <>
@@ -30,10 +48,11 @@ function Navbar() {
                                 About
                             </Link>
                         </li>
-                        <li className='nav-item'>
-                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                                Services
-                            </Link>
+                        <li className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+                            <div className='nav-links' onClick={closeMobileMenu}>
+                                Services <i className='fas fa-caret-down' />
+                            </div>
+                            {dropdown && <Dropdown/>}
                         </li>
                         <li className='nav-item'>
                             <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
